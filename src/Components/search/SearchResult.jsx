b4d856fc0083dd4ab73bde46165c82b23b95df1d
zop1234 <zop1234@hanmail.net>
2023-06-02 14:50:17 +0900

    refactor/Search

--- src/Components/search/SearchResult.jsx
+++ src/Components/search/SearchResult.jsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import MovieCard from '../MovieCard';
 
-export default function SearchResult({ data, keyword }) {
+function SearchResult({ data, keyword }) {
   return (
     <section>
       <div className="mb-5 text-3xl font-bold text-white">
         {keyword}로 검색한 결과
       </div>
       <section className="flex flex-wrap justify-start">
-        {data.map(x => (
-          <MovieCard movie={x} key={x.id} />
+        {data.map(searchMovie => (
+          <MovieCard movie={searchMovie} key={searchMovie.id} />
         ))}
       </section>
     </section>
   );
 }
+
+export default SearchResult;
--- src/Pages/Search.jsx
+++ src/Pages/Search.jsx
@@ -1,24 +1,25 @@
 import React, { useState, useEffect } from 'react';
 import tw from 'tailwind-styled-components';
+
 import SearchInput from '../Components/search/SearchInput';
 import SearchResult from '../Components/search/SearchResult';
 import SearchSimilar from '../Components/search/SearchSimilar';
+import { searchMovieData } from '../API/movie';
+import { API_KEY } from '../Assets/ConstantValue';
 
-export default function Search() {
+function Search() {
   const [input, setInput] = useState('');
   const [keyword, setKeyword] = useState('');
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   const [genre, setGenre] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
-  const [id, setId] = useState('');
-  const API_KEY = process.env.REACT_APP_TMDB_API_KEY;
 
-  const inputHandler = e => {
+  const Handlerinput = e => {
     setInput(e.target.value);
   };
 
-  const searchHandler = () => {
+  const Handlersearch = () => {
     if (input === keyword) return;
     setKeyword(input);
     setData([]);
@@ -32,19 +33,15 @@ export default function Search() {
     }
     if (isLoading) return;
     setIsLoading(true);
-    fetch(
-      `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=ko&query=${keyword}`,
-    )
+    searchMovieData(API_KEY, keyword)
       .then(res => res.json())
       .then(json => {
         if (!json.results.length) {
           setData(json.results);
-          setId('');
           setIsLoading(false);
           return;
         }
         setData(json.results);
-        setId(json.results[0].id);
         setIsLoading(false);
         setGenre(json.results[0].genre_ids[0]);
       });
@@ -52,29 +49,30 @@ export default function Search() {
 
   return (
     <SearchMain id="search">
-      <SearchInput inputHandler={inputHandler} searchHandler={searchHandler} />
+      <SearchInput Handlerinput={Handlerinput} Handlersearch={Handlersearch} />
       <section className="max-w-[900px]">
-        {!data.length && (
+        {!data.length ? (
           <div className="text-3xl font-bold text-white">
             검색 결과가 없습니다.
           </div>
-        )}
-        {data.length && <SearchResult data={data} keyword={keyword} />}
-        {data.length && id && (
-          <SearchSimilar
-            page={page}
-            setPage={setPage}
-            keyword={keyword}
-            id={id}
-            isLoading={isLoading}
-            setIsLoading={setIsLoading}
-            genre={genre}
-          />
+        ) : (
+          <>
+            <SearchResult data={data} keyword={keyword} />
+            <SearchSimilar
+              page={page}
+              setPage={setPage}
+              isLoading={isLoading}
+              setIsLoading={setIsLoading}
+              genre={genre}
+            />
+          </>
         )}
       </section>
     </SearchMain>
   );
 }
+
+export default Search;
 
 const SearchMain = tw.section`
   flex 
